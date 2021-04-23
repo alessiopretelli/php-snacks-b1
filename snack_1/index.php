ce5c6811527b2e3aa94eb5a142b73017bd1893b6
Alessio <ale79631@gmail.com>
2021-04-23 16:08:26 +0200

    aggiunta proprieta

--- snack_1/index.php
+++ snack_1/index.php
@@ -31,26 +31,54 @@
     // }
 
     // metodo 2 'chiavi di chiavi'
+    // $matches = [
+    //     'Franchi' => [
+    //         'Fiorentina' => 60,
+    //         "Milan" => 10
+    //     ],
+    //     'Castellani' => [
+    //         'Empoli' => 55,
+    //         "Roma" => 30
+    //     ],
+    //     'Santa Elisa' => [
+    //         'Lucca' => 35,
+    //         "Vinci" => 50
+    //     ]
+    // ];
+
+    // $chiavi = array_keys($matches);
+
+    // for ($i = 0; $i < count($chiavi); $i++) {
+    //     $c_chiavi = array_keys($matches[$chiavi[$i]]);
+    //     echo $chiavi[$i] . " " . $c_chiavi[0] . "-" . $c_chiavi[1] . " | " . $matches[$chiavi[$i]][$c_chiavi[0]] . "-" . $matches[$chiavi[$i]][$c_chiavi[1]] . ". ";
+    // }
+    
+    // metodo 3
     $matches = [
         'Franchi' => [
-            'Fiorentina' => 60,
-            "Milan" => 10
+            'squadra_in_casa' => 'Fiorentina',
+            'squadra_avversaria' => 'Milan',
+            'punti_in_casa' => 60,
+            'punti_avversaria' => 10
         ],
         'Castellani' => [
-            'Empoli' => 55,
-            "Roma" => 30
+            'squadra_in_casa' => 'Empoli',
+            'squadra_avversaria' => 'Roma',
+            'punti_in_casa' => 55,
+            'punti_avversaria' => 30
         ],
         'Santa Elisa' => [
-            'Lucca' => 35,
-            "Vinci" => 50
+            'squadra_in_casa' => 'Lucca',
+            'squadra_avversaria' => 'Vinci',
+            'punti_in_casa' => 35,
+            'punti_avversaria' => 50
         ]
     ];
 
     $chiavi = array_keys($matches);
 
     for ($i = 0; $i < count($chiavi); $i++) {
-        $c_chiavi = array_keys($matches[$chiavi[$i]]);
-        echo $chiavi[$i] . " " . $c_chiavi[0] . "-" . $c_chiavi[1] . " | " . $matches[$chiavi[$i]][$c_chiavi[0]] . "-" . $matches[$chiavi[$i]][$c_chiavi[1]] . ". ";
+        echo $chiavi[$i] . " " . $matches[$chiavi[$i]]['squadra_in_casa'] . "-" . $matches[$chiavi[$i]]['squadra_avversaria'] . " | " . $matches[$chiavi[$i]]['punti_in_casa'] . "-" . $matches[$chiavi[$i]]['punti_avversaria'] . ". ";
     }
 
 ?>
